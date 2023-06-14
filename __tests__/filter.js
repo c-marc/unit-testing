73768395d181a7718147744ae0b50dd39046100d
Marc C <mcar176@gmail.com>
2023-06-14 15:31:55 +0200

    mockReturnValue

--- __tests__/filter.js
+++ __tests__/filter.js
@@ -17,9 +17,29 @@ it("filter returns an empty table when predicate is false", () => {
 });
 
 it("filter is called correctly", () => {
-  const tab = [1, 2, 3];
-  // const mockFn = jest.fn();
-  const mockFn = jest.fn((x) => x);
+  const tab = ["a", "b", "c"];
+  const mockFn = jest.fn();
   const result = filter(tab, mockFn);
-  expect(mockFn).toHaveBeenNthCalledWith(3, 1, 2, 3);
+  expect(mockFn).toHaveBeenNthCalledWith(1, "a");
+  expect(mockFn).toHaveBeenNthCalledWith(2, "b");
+  expect(mockFn).toHaveBeenNthCalledWith(3, "c");
+});
+
+describe("filter works correctly", () => {
+  it("filter true once", () => {
+    const tab = ["a", "b", "c"];
+    const filterMock = jest.fn();
+    filterMock.mockReturnValueOnce(true).mockReturnValueOnce(false);
+    const result = filter(tab, filterMock);
+    expect(result).toEqual(["a"]);
+  });
+
+  it("filter false once", () => {
+    const tab = ["a", "b", "c"];
+    const filterMock = jest.fn();
+    filterMock.mockReturnValueOnce(false).mockReturnValueOnce(true);
+    const result = filter(tab, filterMock);
+    console.log(result); // only b
+    expect(result).toEqual(["b", "c"]);
+  });
 });
